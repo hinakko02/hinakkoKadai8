722c4657a0e5983fb911fd493a6d63f377d5707f
akio0911 <akio0911@gmail.com>
2023-06-05 20:31:19 +0900

    feedback

--- HinakkoKadai8/HinakkoKadai8/GreenViewController.swift
+++ HinakkoKadai8/HinakkoKadai8/GreenViewController.swift
@@ -10,30 +10,24 @@ import UIKit
 class GreenViewController :UIViewController {
 
 
-    @IBOutlet weak var greenLabel: UILabel!
+    @IBOutlet weak var label: UILabel!
 
     @IBOutlet weak var slider: UISlider!
     
 
-    @IBAction func changeButton(_ sender: UISlider) {
+    @IBAction func changeSlider(_ sender: UISlider) {
         let delegate = UIApplication.shared.delegate as! AppDelegate
         //AppDelegateに用意した変数shareDataに値を入れる
-        delegate.shareData = "\(slider.value)"
-        greenLabel.text = "\(slider.value)"
+        delegate.shareData = slider.value
+        label.text = "\(slider.value)"
     }
 
     //sliderのポインターの動き
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
 
-        let delegate = UIApplication.shared.delegate as? AppDelegate
-        greenLabel.text = delegate?.shareData
-        delegate?.sliderPoint = slider
-    }
-    //Labelの継承
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
         let delegate = UIApplication.shared.delegate as! AppDelegate
-        delegate.sliderPoint.value = slider.value
+        label.text = "\(delegate.shareData)"
+        slider.value = delegate.shareData
     }
 }
